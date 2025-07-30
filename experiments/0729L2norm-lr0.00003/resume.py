e191a1203d256430ee4a1af8e91710ffef4f8ea4
train <100618029+codeofrina@users.noreply.github.com>
2025-07-30 16:50:01 +0000

    resume-fulldataset-500epoch

---
+++ experiments/0729L2norm-lr0.00003/resume.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+
+# 加载之前的 checkpoint
+model = YOLO("/workspace/yolov11bee/experiments/0729L2norm-lr0.00003/01train2/weights/last.pt")
+
+# 继续训练
+model.train(resume=True)
