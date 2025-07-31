508faec852fa4e07805daf9cc9003d9121b82315
train <100618029+codeofrina@users.noreply.github.com>
2025-07-31 12:21:07 +0000

    syncfullbee24best.pt

--- experiments/0729L2norm-lr0.00003/resume.py
+++ experiments/0729L2norm-lr0.00003/resume.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # 加载之前的 checkpoint
-model = YOLO("/workspace/yolov11bee/experiments/0729L2norm-lr0.00003/01train2/weights/last.pt")
+model = YOLO("/workspace/yolov11bee/experiments/0729L2norm-lr0.00003/01train/weights/last.pt")
 
 # 继续训练
 model.train(resume=True)
